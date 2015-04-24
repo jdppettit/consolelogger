5655cfd4b005b0a2bdc6b784e4dba994faaa5daa
Zack Buhman <zack@buhman.org>
2015-04-23 23:16:30 -0400

    tweak example documentation; svg preview

--- examples/examples.js
+++ examples/examples.js
@@ -1,13 +1,8 @@
 var log = require('consolelogger').log;
 
 log("This is error", "error");
-
 log("This is warning", "warning");
-
 log("This is success", "success");
-
 log("This is debug", "debug");
-
 log("This is info", "info");
-
 log("This is a regular message");
