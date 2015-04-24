3ffa84c32011553f9da59ba683b3a8e9b211e73c
Joe Pettit <jpettit@pettitservers.com>
2015-04-23 20:12:45 -0500

    Added examples

---
+++ examples/examples.js
@@ -0,0 +1,13 @@
+var log = require('./logger').log;
+
+log("This is error", "error");
+
+log("This is warning", "warning");
+
+log("This is success", "success");
+
+log("This is debug", "debug");
+
+log("This is info", "info");
+
+log("This is a regular message");
